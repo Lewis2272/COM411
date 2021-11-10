cba1d50fadb6a93ea02eea98dc67f235db11eaff
Lewis <4rogel85@solent.ac.uk>
2021-11-10 06:57:50 +0000

    Added code to demonstrate the use of a simple for loop

---
+++ Basics/Repetitions/for_loop/simple.py
@@ -0,0 +1,13 @@
+print("How many mountains should I display?")
+user_num = int(input())
+
+# Count declared as control variable, stop, start and step can be changed
+for count in range(user_num):
+    print("__")
+    print("/  \_")
+    print("/^    \^")
+    print("/  ^    \_")
+    print("_/ ^ ^     ^\^")
+    print("/  ^     ^    \^")
+
+print("Done!")
